dab6a1a637bf69c02ac7e78396c24e6e1a72fcd6
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-02-22 12:06:50 -0300

    chore(handleNotFound.js) remove debug commands

--- src/middlewares/handleNotFound.js
+++ src/middlewares/handleNotFound.js
@@ -1,10 +1,6 @@
 const createError = require('http-errors');
-const debug = require('debug')('app:middlewares:handleNotFound')
-
-debug('On handleNotFound.js')
 
 function handleNotFound(req, res, next) {
-  debug('Handling Not Found...')
   next(createError(404, `Endpoint ${req.url} not found`));
 }
 
