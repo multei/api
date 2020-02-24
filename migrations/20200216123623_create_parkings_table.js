ef59c6ce8631853f8eba522ceedcefa5b699857f
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-02-24 11:22:16 -0300

    chore(migrations/*.js) make parkings table fields nullable

--- migrations/20200216123623_create_parkings_table.js
+++ migrations/20200216123623_create_parkings_table.js
@@ -9,10 +9,12 @@ exports.up = (knex, Promise) => knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-o
       table.increments('id').notNullable().primary().unique();
       table.uuid('uuid').defaultTo(knex.raw('uuid_generate_v4()')).unique();
 
-      table.string('car_color').notNullable();
-      table.string('car_make').notNullable();
-      table.string('car_make_model').notNullable();
-      table.string('car_plate', 7).notNullable();
+      table.string('car_color');
+      table.string('car_make');
+      table.string('car_make_model');
+      table.string('car_plate', 7);
+      table.string('car_front_photo_uri', 2083);
+      table.string('car_rear_photo_uri', 2083);
 
       table.specificType('coordinates', 'POINT').nullable();
 
