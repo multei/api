f8aaeb6951cdd61a6fb4ffa90b858aec51d531c6
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-02-25 18:02:30 -0300

    fix(knexfile.js) add knex on test environments

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,11 @@ module.exports = {
     connection: process.env.DATABASE_URL
   },
 
+  test: {
+    client: 'pg',
+    connect: process.env.DATABASE_URL
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
