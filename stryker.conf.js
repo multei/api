d05aa4f480f922ddce94a3b8c9611f6e67d46ffe
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-11 23:31:16 -0300

    tests(stryker.conf.js): upgrade config file to stryker v3

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,27 +1,18 @@
-const glob = require('glob')
-
-module.exports = function(config) {
-  config.set({
-    coverageAnalysis: 'off',
-    files: ['__mocks__/**/*.js', 'package.json', 'src/**/*.js', '!src/**/*.e2e.test.js'],
-    jest: {
-      config: require('./jest.config')
-    },
-    mutate: [
-      'src/**/*.js',
-      '!src/**/*.e2e.test.js',
-      '!src/**/*.unit.test.js',
-      '!__mocks__/**/*.js'
-    ],
-    mutator: "javascript",
-    packageManager: "npm",
-    reporters: [
-      "clear-text",
-      "dashboard",
-      "html",
-      "progress",
-    ],
-    // testRunner: "jest",
-    transpilers: [],
-  });
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  coverageAnalysis: "off",
+  files: ['__mocks__/**/*.js', 'package.json', 'src/**/*.js', '!src/**/*.e2e.test.js'],
+  mutate: [
+    'src/**/*.js',
+    '!src/**/*.e2e.test.js',
+    '!src/**/*.unit.test.js',
+    '!__mocks__/**/*.js'
+  ],
+  mutator: "javascript",
+  packageManager: "npm",
+  reporters: ["html", "clear-text", "progress", "dashboard"],
+  testRunner: "jest",
+  transpilers: []
 };
