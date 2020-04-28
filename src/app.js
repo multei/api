1f7032ed3530765819d0c2ec7075cdf07da89373
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-04-28 15:42:15 -0300

    fx(app.js): move requires to top

--- src/app.js
+++ src/app.js
@@ -3,7 +3,17 @@ const debug = require('debug')('app')
 const dotenv = require('dotenv')
 const express = require('express');
 const logger = require('morgan');
+
 const { ExpressMiddleware } = require('express-api-problem')
+const expressMiddleware = ExpressMiddleware()
+
+const addHeadersMiddleware = require('./api/middlewares/addHeaders')
+const corsMiddleware = require('./api/middlewares/cors')
+const notFoundHandler = require('./api/middlewares/handleNotFound')
+const serverErrorsHandler = require('./api/middlewares/handleServerErrors')
+
+const apiRootRouteHandler = require('./api/routes')
+const v1RouteHandler = require('./api/routes/v1')
 
 debug('On app.js file')
 dotenv.config()
@@ -14,26 +24,25 @@ app.disable('etag')
 app.disable('x-powered-by')
 app.use(logger('dev'));
 app.use(cookieParser());
-app.use(require('./api/middlewares/addHeaders'))
+app.use(addHeadersMiddleware)
 
 debug('Configuring CORS...')
-app.use(require('./api/middlewares/cors'))
+app.use(corsMiddleware)
 
 debug('Configuring routes for /...')
-app.use('/', require('./api/routes'));
+app.use('/', apiRootRouteHandler);
 
 debug('Configuring routes for /v1/*...')
-app.use('/v1', require('./api/routes/v1'));
+app.use('/v1', v1RouteHandler);
 
 debug('Configuring middleware for handling Not Found error...')
-app.use(require('./api/middlewares/handleNotFound'))
+app.use(notFoundHandler)
 
 debug('Configuring middleware for handling another server errors...')
-app.use(require('./api/middlewares/handleServerErrors'))
+app.use(serverErrorsHandler)
 
 debug('Configuring middleware for generating problem+json errors...')
-app.use(ExpressMiddleware())
+app.use(expressMiddleware)
 
 debug('After configure API on app.js')
-
 module.exports = app;
